b2ed7447604d18a29e04a36744f898ccb74a794d
Islam Farg <islam3zzat@gmail.com>
2024-05-28 08:11:48 +0100

    Install .app bundle in home directory to avoid dylib ID changes

--- Formula/k8s-navigator.rb
+++ Formula/k8s-navigator.rb
@@ -10,29 +10,29 @@ class K8sNavigator < Formula
   def install
     # Extract the .app bundle
     system "unzip", cached_download, "-d", "extracted"
-    # Install the entire .app bundle in the prefix directory
-    prefix.install "extracted/k8s-navigator.app"
+    # Create Applications directory if it doesn't exist
+    applications_dir = "/Applications/k8s-navigator.app"
+    system "mkdir", "-p", applications_dir
+    # Install the entire .app bundle in the Applications directory
+    system "cp", "-r", "extracted/k8s-navigator.app", applications_dir
     # Create a symlink to the main executable
-    bin.install_symlink prefix/"k8s-navigator.app/Contents/MacOS/k8s-navigator" => "k8s-navigator"
+    bin.install_symlink "#{applications_dir}/Contents/MacOS/k8s-navigator" => "k8s-navigator"
   end
 
   def caveats
     <<~EOS
-      The k8s-navigator app bundle has been installed and signed in:
-        #{opt_prefix}/k8s-navigator.app
+    The k8s-navigator app bundle has been installed in:
+    /Applications/k8s-navigator.app
 
-      To add the app to your Applications folder, you need to create a symlink manually:
-        sudo ln -s #{opt_prefix}/k8s-navigator.app /Applications/k8s-navigator.app
+    To add the executable to your PATH, you can add the following line to your shell configuration:
+      echo 'export PATH="/Applications/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
 
-      To add the executable to your PATH, you can add the following line to your shell configuration:
-        echo 'export PATH="#{opt_prefix}/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
+    After that, you can run the app by typing:
+      k8s-navigator
 
-        After that, you can run the app by typing:
-        k8s-navigator
+    or, to run it in the background:
 
-        or, to run it in the background:
-
-        k8s-navigator &
+    k8s-navigator &
     EOS
   end
 

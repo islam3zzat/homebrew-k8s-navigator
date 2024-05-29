6b78f952e9e18cd2dcffd03358e795772ef54bfa
Islam Farg <islam3zzat@gmail.com>
2024-05-29 20:30:28 +0100

    update formula

--- Formula/k8s-navigator.rb
+++ Formula/k8s-navigator.rb
@@ -1,8 +1,8 @@
 class K8sNavigator < Formula
   desc "K8s Navigator is a tool for navigating Kubernetes clusters"
   homepage "https://github.com/islam3zzat/k8s-navigator"
-  url "https://storage.googleapis.com/k8s-navigator-bucket/k8s-navigator-darwin-arm64-0.0.3.zip"
-  sha256 "5dae9b0fc459e11c9e933d75886ce96a7f0984fe57377a18040c85c33102dab1"
+  url "https://storage.googleapis.com/k8s-navigator-bucket/k8s-navigator-darwin-arm64-0.0.22.zip"
+  sha256 "694abb6f68323bd9c95034556047c9c069919cd6d83fd214365a2021349b1a60"
   license "MIT"
 
   depends_on "node"
@@ -11,27 +11,19 @@ class K8sNavigator < Formula
     system "unzip", cached_download, "-d", "extracted"
     # Install the entire .app bundle in the prefix directory
     prefix.install "extracted/k8s-navigator.app"
-    # Create a symlink to the main executable
-    bin.install_symlink prefix/"k8s-navigator.app/Contents/MacOS/k8s-navigator" => "k8s-navigator"
   end
 
   def caveats
     <<~EOS
-    The k8s-navigator app bundle has been installed in:
-    #{opt_prefix}/k8s-navigator.app
+    To launch k8s-navigator, you can either:
+    - Open Finder, navigate to #{opt_prefix}, and double-click k8s-navigator.app
+    - Or use the command: open #{opt_prefix}/k8s-navigator.app
 
-  To move the app to your Applications folder, you can run:
-    sudo mv #{opt_prefix}/k8s-navigator.app /Applications/
+    To add k8s-navigator to your Applications folder, use the command:
+      cp -r #{opt_prefix}/k8s-navigator.app /Applications
 
-  To add the executable to your PATH, you can add the following line to your shell configuration:
-    echo 'export PATH="/Applications/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
-
-  After that, you can run the app by typing:
-    k8s-navigator
-
-  or, to run it in the background:
-
-    k8s-navigator &
+    to add k8s-navigator to your PATH, use the command:
+      echo 'export PATH="#{opt_prefix}/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
 EOS
   end
 

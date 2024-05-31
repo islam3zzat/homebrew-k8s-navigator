0564853d46243b76ec01c9ed557af63e5ed3d320
Islam Farg <islam3zzat@gmail.com>
2024-05-31 17:50:54 +0100

    Fix dylib ID changes

--- Formula/k8s-navigator.rb
+++ Formula/k8s-navigator.rb
@@ -1,8 +1,8 @@
 class K8sNavigator < Formula
   desc "K8s Navigator is a tool for navigating Kubernetes clusters"
   homepage "https://github.com/islam3zzat/k8s-navigator"
-  url "https://storage.googleapis.com/k8s-navigator-bucket/k8s-navigator-darwin-arm64-0.0.22.zip"
-  sha256 "694abb6f68323bd9c95034556047c9c069919cd6d83fd214365a2021349b1a60"
+  url "https://storage.googleapis.com/k8s-navigator-bucket/k8s-navigator-darwin-universal-0.1.99"
+  sha256 "6913d00c25831b78e7eb1ea4fbe33635feca6c697134582dccf155585d0f29b4"
   license "MIT"
 
   depends_on "node"
@@ -11,19 +11,20 @@ class K8sNavigator < Formula
     system "unzip", cached_download, "-d", "extracted"
     # Install the entire .app bundle in the prefix directory
     prefix.install "extracted/k8s-navigator.app"
+    # Copy the .app bundle to the Applications directory
+    system "cp", "-r", "#{prefix}/k8s-navigator.app", "/Applications"
   end
 
   def caveats
     <<~EOS
+    k8s-navigator has been installed in your Applications folder.
+
     To launch k8s-navigator, you can either:
-    - Open Finder, navigate to #{opt_prefix}, and double-click k8s-navigator.app
-    - Or use the command: open #{opt_prefix}/k8s-navigator.app
+    - Open Finder, navigate to /Applications, and double-click k8s-navigator.app
+    - Or use the command: open /Applications/k8s-navigator.app
 
-    To add k8s-navigator to your Applications folder, use the command:
-      cp -r #{opt_prefix}/k8s-navigator.app /Applications
-
-    to add k8s-navigator to your PATH, use the command:
-      echo 'export PATH="#{opt_prefix}/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
+    To add k8s-navigator to your PATH, use the command:
+      echo 'export PATH="/Applications/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
 EOS
   end
 

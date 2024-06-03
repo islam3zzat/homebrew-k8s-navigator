532b0f21e29ecc2bd51458abbd45e6b13c475dbf
Islam Farg <islam3zzat@gmail.com>
2024-06-03 18:37:41 +0100

    update cask

--- Casks/k8s-navigator.rb
+++ Casks/k8s-navigator.rb
@@ -2,7 +2,7 @@ cask "k8s-navigator" do
   version "0.0.41"
   sha256 "9f3e60b92994f92a81ea44df8792c70fcceba4513f5521da22acfe00b212acd3"
 
-  url "https://github.com/islam3zzat/k8s-navigator/releases/download/#{version}/k8s-navigator-0.0.41-universal.dmg"
+  url "https://github.com/islam3zzat/k8s-navigator/releases/download/#{version}/k8s-navigator-#{version}-universal.dmg"
   name "K8s Navigator"
   desc "Tool for navigating Kubernetes clusters"
   homepage "https://github.com/islam3zzat/k8s-navigator"

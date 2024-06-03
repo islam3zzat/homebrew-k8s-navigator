c0bbf02d6d649b7f3376597361fbdc0cc4a19962
Islam Farg <islam3zzat@gmail.com>
2024-06-03 18:42:44 +0100

    update cask

--- Casks/k8s-navigator.rb
+++ Casks/k8s-navigator.rb
@@ -11,6 +11,12 @@ cask "k8s-navigator" do
 
   app "k8s-navigator.app"
 
+  zap trash: [
+    "~/Library/Application Support/k8s-navigator",
+    "~/Library/Preferences/com.example.k8s-navigator.plist",
+    "~/Library/Saved Application State/com.example.k8s-navigator.savedState",
+  ]
+
   caveats <<~EOS
     To add k8s-navigator to your PATH, you can use the following command:
       echo 'export PATH="/Applications/k8s-navigator.app/Contents/MacOS:$PATH"' >> ~/.zshrc
@@ -20,12 +26,6 @@ cask "k8s-navigator" do
 
     You will need to restart your terminal for the changes to take effect.
   EOS
-
-  zap trash: [
-    "~/Library/Application Support/k8s-navigator",
-    "~/Library/Preferences/com.example.k8s-navigator.plist",
-    "~/Library/Saved Application State/com.example.k8s-navigator.savedState",
-  ]
 
   test do
     system "/Applications/k8s-navigator.app/Contents/MacOS/k8s-navigator", "--version"

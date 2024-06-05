8f7473c507bc85e9f35cd255b8ea77b508fe0d1a
Islam Farg <islam3zzat@gmail.com>
2024-06-05 19:30:16 +0100

    update cask

--- Casks/k8s-navigator.rb
+++ Casks/k8s-navigator.rb
@@ -1,6 +1,6 @@
 cask "k8s-navigator" do
   version "0.0.52"
-  sha256 "c79ee0512ee03c5ac82a86dc34905d51a7d091f10ade2e76c508fd4f7042533d"
+  sha256 "21db6548f3c09537bdd9f17f4de2540a268b29516e291147d4df083bef530676"
 
   url "https://github.com/islam3zzat/k8s-navigator/releases/download/#{version}/k8s-navigator-#{version}-universal.dmg"
   name "K8s Navigator"
